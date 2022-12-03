4d89abb0738628f201ce5665798ef441df82478c
Wappen <lenz.koxholt@gmail.com>
2022-12-03 16:13:36 +0100

    fix anagram id hashing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use std::collections::{BTreeMap, HashMap};
+use countmap::CountMap;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::path::Path;
 use std::{fs, io};
 
@@ -6,17 +8,37 @@ pub trait Anagrams {
     fn get_all(&self, word: &str) -> Option<&Vec<String>>;
 }
 
-#[derive(Eq, PartialEq, Hash)]
 struct AnagramID {
-    count_map: BTreeMap<char, usize>,
+    count_map: CountMap<char>,
+}
+
+impl PartialEq<Self> for AnagramID {
+    fn eq(&self, other: &Self) -> bool {
+        return self.count_map.len() == other.count_map.len()
+            && self.count_map.keys().eq(other.count_map.keys())
+            && self.count_map.values().eq(other.count_map.values());
+    }
+}
+
+impl Eq for AnagramID {}
+
+impl Hash for AnagramID {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.count_map.len().hash(state);
+
+        for (char, count) in self.count_map.iter() {
+            char.hash(state);
+            count.hash(state);
+        }
+    }
 }
 
 impl From<&str> for AnagramID {
     fn from(str: &str) -> Self {
-        let mut count_map: BTreeMap<char, usize> = BTreeMap::new();
+        let mut count_map: CountMap<char> = CountMap::new();
 
         for c in str.chars() {
-            count_map.insert(c, *count_map.get(&c).unwrap_or(&1));
+            count_map.insert_or_increment(c);
         }
 
         AnagramID { count_map }
